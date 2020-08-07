d74509cac627034be9f6fa464a941b55ce5506da
Dmitry Sushchenkov <suchenkov.d@gmail.com>
2020-08-07 19:57:07 +1000

    master

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -137,6 +137,31 @@ $(document).ready(function () {
         speed: 300,
         slidesToShow: 5,
         slidesToScroll: 2,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
     });
 
     $('.btn-prev').on('click', function () {
